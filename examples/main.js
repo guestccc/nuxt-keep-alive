b093f6ce0849528c223380bdaa816bd0a29fa796
“ccc <1009078602@qq.com>
2019-10-14 14:09:00 +0800

    :tada: first commit

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import store from './store'
 
 // 导入组件库
@@ -11,7 +11,7 @@ Vue.use(Nkalive)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  // router,
   store,
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,5 @@
 module.exports = {
-  // devServer: {
-  //   port: 8081, // 端口号
-  //   host: "localhost",
-  //   https: false, // https:{type:Boolean}
-  //   open: true, //配置自动启动浏览器
-  //   // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
-  //   proxy: {
-  //     "/api": {
-  //       target: "<url>",
-  //       ws: true,
-  //       changeOrigin: true
-  //     },
-  //     "/foo": {
-  //       target: "<other_url>"
-  //     }
-  //   } // 配置多个代理
-  // },
+  // 修改 src 为 examples
   pages: {
     index: {
       entry: 'examples/main.js',
@@ -23,12 +7,14 @@ module.exports = {
       filename: 'index.html'
     }
   },
+  // 强制内联CSS
+  // css: { extract: false },
   // 扩展 webpack 配置，使 packages 加入编译
   chainWebpack: config => {
     config.module
-      .rule('js')
+      .rule('.js')
       .include
-        .add('packages')
+        .add("/packages")
         .end()
       .use('babel')
         .loader('babel-loader')
@@ -37,4 +23,4 @@ module.exports = {
           return options
         })
   }
-};
+}
